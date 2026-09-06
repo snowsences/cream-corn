40db0e5658c110d59a1614a5818272c75adeff66
Kevin Allen <allenkevinc@gmail.com>
2026-09-06 13:05:08 -0700

    1

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='cream-corn-v1';self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request))));
